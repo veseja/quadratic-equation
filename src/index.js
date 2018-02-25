103a9f003c5b2465ea605a686a6a3cc73eaac340
Liubou Shchupak <36640822+veseja@users.noreply.github.com>
2018-02-26 02:47:03 +0300

    sol1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 module.exports = function solveEquation(equation) {
-    var t = equation.replace("x^2", "Math.pow(x,2)");
-    var s = [];
-    var count = 0;
-    for (var x = -4999999; x < 4999999; x++) {
-        if (eval(t) == 0) {
-            s[count] = x;
-            count++;
-            if (count == 2) break;
-        }
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a < b) return -1;
     }
-    return s;
+
+    equation = equation.replace("x^2 - ", "x^2 -");
+    equation = equation.replace("x^2 + ", "x^2 +");
+    equation = equation.replace("* x + ", "* x +");
+    equation = equation.replace("* x - ", "* x -");
+    var s = [];
+    s = equation.split(" ");
+    var a = Number(s[0]);
+    var b = Number(s[3]);
+    var c = Number(s[6]);
+
+    var d = Math.pow(b, 2) - 4 * a * c;
+    var x1 = (-b + Math.sqrt(d)) / (2 * a);
+    var x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+    var st = [];
+    st[0] = Math.round(x1);
+    st[1] = Math.round(x2);
+    st.sort(compareNumeric);
+    return st;
 }
