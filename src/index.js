bb960c005463622036e932ab007dd18bbce0d10a
Liubou Shchupak <36640822+veseja@users.noreply.github.com>
2018-02-26 01:52:45 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var t = equation.replace("x^2", "Math.pow(x,2)");
+    var s = [];
+    var count = 0;
+    for (var x = -4999999; x < 4999999; x++) {
+        if (eval(t) == 0) {
+            s[count] = x;
+            count++;
+            if (count == 2) break;
+        }
+    }
+    return s;
 }
